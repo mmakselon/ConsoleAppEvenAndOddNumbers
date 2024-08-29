ca198da47474148b0151eb0cd7c600b56e5a89d2
mmakselon <magdalena.pachacz@gmail.com>
2024-08-29 10:34:09 +0200

    initial commit

---
+++ ConsoleAppEvenAndOddNumbers/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEvenAndOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę:");
+
+            while (true)
+            {
+                var number = GetNumber(Console.ReadLine());
+                CheckNumber(number);
+
+                Console.WriteLine("Jeżeli chcesz podaj kolejną liczbę. Jeśli chcesz zakończyć kliknij 'q'.");
+
+            }
+
+
+            Console.WriteLine();
+        }
+
+
+        static int GetNumber(string number)
+        {
+
+
+            while (true)
+            {
+                if (number.ToLower() == "q")
+                    Environment.Exit(0);
+
+                if (!int.TryParse(number, out int result))
+                {
+                    Console.WriteLine("Podałeś nieprawidłową wartość!");
+                    continue;
+                }
+                else
+                    return result;
+            }
+        }
+
+        static void CheckNumber(int number)
+        {
+
+            if (number % 2 == 0)
+                Console.WriteLine("Podałeś liczbę parzystą.");
+            else
+                Console.WriteLine("Podałeś liczbę nieparzystą.");
+
+        }
+
+    }
+}
